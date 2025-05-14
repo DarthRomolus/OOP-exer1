18e2b939555e2318c6ee96fc2a51004a8f74994a
DarthRomolus <rom.mail2005@gmail.com>
2025-05-14 20:48:02 +0300

    finished the ProductController

--- src/main/java/com/example/exer1/entity/Product.java
+++ src/main/java/com/example/exer1/entity/Product.java
@@ -29,4 +29,43 @@ public class Product {
     private int quantity;
 
 
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description=description;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id=id;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
